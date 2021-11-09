390043457b07590d3bed09a629d66af26dcdeac2
Simeon Griggs <simeon@hey.com>
2021-11-09 09:17:13 +0000

    public live preview

--- studio/src/lib/resolveProductionUrl.js
+++ studio/src/lib/resolveProductionUrl.js
@@ -1,6 +1,10 @@
+const localUrl = `http://localhost:3000`
+const remoteUrl = `https://fragrant-resonance-1495.fly.dev`
+const baseUrl = window?.location?.hostname === 'localhost' ? localUrl : remoteUrl
+
 export function resolveProductionUrl(doc) {
   const slug = doc?.slug?.current
-  const url = new URL(`http://localhost:3000`)
+  const url = new URL(baseUrl)
   url.pathname = slug
   url.searchParams.set(`preview`, `true`)
 
