db8812631c777a92072596490ac1a2a49d232a7e
Simeon Griggs <simeon@hey.com>
2021-11-12 08:52:47 +0000

    update url

--- studio/src/lib/resolveProductionUrl.js
+++ studio/src/lib/resolveProductionUrl.js
@@ -1,5 +1,5 @@
 const localUrl = `http://localhost:3000`
-const remoteUrl = `https://remix.simeongriggs.dev`
+const remoteUrl = `https://www.simeongriggs.dev`
 const baseUrl = window?.location?.hostname === 'localhost' ? localUrl : remoteUrl
 
 export function resolveProductionUrl(doc) {
