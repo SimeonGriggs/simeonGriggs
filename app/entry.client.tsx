05185a71040506e495df908ffa7d31ef0c785c9a
Simeon Griggs <simeon@hey.com>
2023-10-03 18:43:14 +0100

    update entries

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,12 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+/**
+ * By default, Remix will handle hydrating your app on the client for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.client
+ */
+
+import {RemixBrowser} from '@remix-run/react'
+import {startTransition, StrictMode} from 'react'
+import {hydrateRoot} from 'react-dom/client'
 
 startTransition(() => {
   hydrateRoot(
@@ -8,5 +14,5 @@ startTransition(() => {
     <StrictMode>
       <RemixBrowser />
     </StrictMode>
-  );
-});
+  )
+})
