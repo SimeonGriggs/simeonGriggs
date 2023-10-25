5d46c5f6d7ddef150c1b774aaa427b3bcb6ecde7
Simeon Griggs <simeon@hey.com>
2023-10-25 17:09:17 +0100

    allow login

--- app/components/StudioWrapper.tsx
+++ app/components/StudioWrapper.tsx
@@ -1,12 +1,15 @@
+import {useSearchParams} from '@remix-run/react'
 import {Studio} from 'sanity'
 
 import {Hydrated} from '~/components/Hydrated'
 import {config} from '~/sanity/config'
 
 export default function StudioWrapper() {
+  const [searchParams] = useSearchParams()
+
   return (
     <Hydrated>
-      <Studio config={config} unstable_noAuthBoundary />
+      <Studio config={config} unstable_noAuthBoundary={searchParams.get('login') !== 'true'} />
     </Hydrated>
   )
 }
--- app/sanity/components/OGPreview.tsx
+++ app/sanity/components/OGPreview.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 export default function OGPreview({document, options}: any) {
   const {displayed} = document
+  // @ts-expect-error
   const url = React.useMemo(() => options.url(displayed), [displayed, options])
 
   return (
