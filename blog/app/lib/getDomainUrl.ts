6e9e17613aa72f668d28aff54537aac35e496138
Simeon Griggs <simeon@hey.com>
2025-09-01 08:30:53 +0100

    update prod url

--- blog/app/lib/getDomainUrl.ts
+++ blog/app/lib/getDomainUrl.ts
@@ -3,10 +3,14 @@
  * https://github.com/kentcdodds/kentcdodds.com/blob/main/app/utils/misc.tsx
  */
 export function getDomainUrl(request: Request) {
-  const host = request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+  const host =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
   if (!host) {
     throw new Error('Could not determine domain URL.')
   }
   const protocol = host.includes('localhost') ? 'http' : 'https'
-  return `${protocol}://${host}`
+  const productionHost = host.includes('vercel.app')
+    ? 'www.simeongriggs.dev'
+    : host
+  return `${protocol}://${productionHost}`
 }
