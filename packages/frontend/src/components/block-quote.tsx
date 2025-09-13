0fd46dcad3f56cff8d6b3887b4a29c8361954953
SimeonGriggs <simeon@hey.com>
2025-09-13 06:37:51 +0100

    setup syncpack

--- packages/frontend/src/components/block-quote.tsx
+++ packages/frontend/src/components/block-quote.tsx
@@ -9,7 +9,7 @@ export function BlockQuote({children, className, ...rest}: BlockQuoteProps) {
   return (
     <blockquote
       className={clsx(
-        'my-10 border-l-2 border-l-gray-300 pl-6 text-base/8 text-gray-950 first:mt-0 last:mb-0',
+        'my-10 border-l-2 border-l-gray-300 pl-6 text-base/8 text-gray-950 first:mt-0 last:mb-0 dark:border-l-gray-600 dark:text-gray-50',
         className,
       )}
       {...rest}
